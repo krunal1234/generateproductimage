d7606d8f855fbcac34d4fe91b73f0f4d275a6759
Makwana Krunal <33956250+krunal1234@users.noreply.github.com>
2025-04-19 08:58:53 +0530

    Update utilities.py

--- rmbg-ai/utilities.py
+++ rmbg-ai/utilities.py
@@ -1,25 +1,19 @@
 import torch
-import torch.nn.functional as F
-from torchvision.transforms.functional import normalize
+import torchvision.transforms as transforms
 import numpy as np
+from PIL import Image
 
-def preprocess_image(im: np.ndarray, model_input_size: list) -> torch.Tensor:
-    if len(im.shape) < 3:
-        im = im[:, :, np.newaxis]
-    # orig_im_size=im.shape[0:2]
-    im_tensor = torch.tensor(im, dtype=torch.float32).permute(2,0,1)
-    im_tensor = F.interpolate(torch.unsqueeze(im_tensor,0), size=model_input_size, mode='bilinear').type(torch.uint8)
-    image = torch.divide(im_tensor,255.0)
-    image = normalize(image,[0.5,0.5,0.5],[1.0,1.0,1.0])
-    return image
+def preprocess_image(image: np.ndarray, target_size=(1024, 1024)):
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize(target_size),
+        transforms.ToTensor()
+    ])
+    tensor = transform(image).unsqueeze(0)  # Add batch dimension
+    return tensor
 
-
-def postprocess_image(result: torch.Tensor, im_size: list)-> np.ndarray:
-    result = torch.squeeze(F.interpolate(result, size=im_size, mode='bilinear') ,0)
-    ma = torch.max(result)
-    mi = torch.min(result)
-    result = (result-mi)/(ma-mi)
-    im_array = (result*255).permute(1,2,0).cpu().data.numpy().astype(np.uint8)
-    im_array = np.squeeze(im_array)
-    return im_array
-    
+def postprocess_image(mask: torch.Tensor, original_size):
+    mask = transforms.Resize(original_size)(mask.unsqueeze(0))
+    mask = mask.squeeze().cpu().detach().numpy()
+    mask = (mask * 255).astype(np.uint8)
+    return mask
