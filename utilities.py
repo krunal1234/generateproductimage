1b2cc5157a4fe11aa6926b9e144f02b56e62b536
Makwana Krunal <33956250+krunal1234@users.noreply.github.com>
2025-04-19 14:41:22 +0530

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,26 +1,25 @@
 import torch
-import torchvision.transforms as transforms
-import numpy as np
-from PIL import Image
 import torch.nn.functional as F
+from torchvision.transforms.functional import normalize
+import numpy as np
 
-def preprocess_image(image: np.ndarray, target_size=(1024, 1024)):
-    transform = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize(target_size),
-        transforms.ToTensor()
-    ])
-    tensor = transform(image).unsqueeze(0)  # Add batch dimension
-    return tensor
+def preprocess_image(im: np.ndarray, model_input_size: list) -> torch.Tensor:
+    if len(im.shape) < 3:
+        im = im[:, :, np.newaxis]
+    # orig_im_size=im.shape[0:2]
+    im_tensor = torch.tensor(im, dtype=torch.float32).permute(2,0,1)
+    im_tensor = F.interpolate(torch.unsqueeze(im_tensor,0), size=model_input_size, mode='bilinear').type(torch.uint8)
+    image = torch.divide(im_tensor,255.0)
+    image = normalize(image,[0.5,0.5,0.5],[1.0,1.0,1.0])
+    return image
 
-def postprocess_image(prediction, orig_shape):
-    # Ensure input is 4D (N, C, H, W)
-    if prediction.dim() == 2:
-        prediction = prediction.unsqueeze(0).unsqueeze(0)
-    elif prediction.dim() == 3:
-        prediction = prediction.unsqueeze(0)
+
+def postprocess_image(result: torch.Tensor, im_size: list)-> np.ndarray:
+    result = torch.squeeze(F.interpolate(result, size=im_size, mode='bilinear') ,0)
+    ma = torch.max(result)
+    mi = torch.min(result)
+    result = (result-mi)/(ma-mi)
+    im_array = (result*255).permute(1,2,0).cpu().data.numpy().astype(np.uint8)
+    im_array = np.squeeze(im_array)
+    return im_array
     
-    resized = F.interpolate(prediction, size=orig_shape, mode='bilinear', align_corners=False)
-    mask = resized.squeeze().cpu().detach().numpy() * 255
-    mask = np.clip(mask, 0, 255).astype(np.uint8)
-    return mask
