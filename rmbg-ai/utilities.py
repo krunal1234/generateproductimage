2c8e9b66f73734ae30f6c4225ad08cf60ca0bc90
Makwana Krunal <33956250+krunal1234@users.noreply.github.com>
2025-04-19 15:59:05 +0530

    Update utilities.py

--- rmbg-ai/utilities.py
+++ rmbg-ai/utilities.py
@@ -1,19 +1,25 @@
 import torch
-import torchvision.transforms as transforms
+import torch.nn.functional as F
+from torchvision.transforms.functional import normalize
 import numpy as np
-from PIL import Image
-import cv2
 
-# Suggested preprocess enhancement
-def preprocess_image(image: np.ndarray, size: List[int]) -> torch.Tensor:
-    image = cv2.resize(image, (size[1], size[0]), interpolation=cv2.INTER_CUBIC)
-    image = image.astype(np.float32) / 255.0
-    image = image.transpose((2, 0, 1))  # HWC to CHW
-    image = torch.from_numpy(image).unsqueeze(0)
+def preprocess_image(im: np.ndarray, model_input_size: list) -> torch.Tensor:
+    if len(im.shape) < 3:
+        im = im[:, :, np.newaxis]
+    # orig_im_size=im.shape[0:2]
+    im_tensor = torch.tensor(im, dtype=torch.float32).permute(2,0,1)
+    im_tensor = F.interpolate(torch.unsqueeze(im_tensor,0), size=model_input_size, mode='bilinear').type(torch.uint8)
+    image = torch.divide(im_tensor,255.0)
+    image = normalize(image,[0.5,0.5,0.5],[1.0,1.0,1.0])
     return image
 
-def postprocess_image(mask: torch.Tensor, original_size):
-    mask = transforms.Resize(original_size)(mask.unsqueeze(0))
-    mask = mask.squeeze().cpu().detach().numpy()
-    mask = (mask * 255).astype(np.uint8)
-    return mask
+
+def postprocess_image(result: torch.Tensor, im_size: list)-> np.ndarray:
+    result = torch.squeeze(F.interpolate(result, size=im_size, mode='bilinear') ,0)
+    ma = torch.max(result)
+    mi = torch.min(result)
+    result = (result-mi)/(ma-mi)
+    im_array = (result*255).permute(1,2,0).cpu().data.numpy().astype(np.uint8)
+    im_array = np.squeeze(im_array)
+    return im_array
+    
